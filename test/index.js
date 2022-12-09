82f6fa95262889d433d106b09064b5cdddb2dff1
RazorHex <ahda043@gmail.com>
2022-12-09 16:04:24 +0700

    deleting test folder

--- test/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-
-axios.get('http://127.0.0.1:5000/dana')
-  .then(res => {
-    const listed_data = res.data
-    listed_data.forEach(data => {
-      console.log(data['Nama Lengkap'])
-    });
-  })
