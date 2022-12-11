e50dbd4f37e851ac8661f1de2dcba49d4b314352
Zaidan Mahdigit config --global user.emailzaidanici@gmail.com <zaidanici@gmail.com>
2022-12-11 14:47:18 +0700

    update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -4,21 +4,32 @@ import data_pembimbing as dt
 import loadjsoncaas as lj
 import pickle
 import numpy as np
-
+import streamlit as st
+import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('dataset.pkl','rb'))
 simi = pickle.load(open('similarity.pkl','rb'))
 ml_list = np.array(model['Skill'])
-@app.route('/mlnya',methods=['POST'])
+option = st.selectbox("Input Skill : ",(ml_list))
+
+@app.route('/mlnya',methods=['POST','GET'])
 def recomen(skill):
-     index = model[model['title'] == skill].index[0]
-     distances = sorted(list(enumerate(simi[index])), reverse=True, key=lambda x: x[1])
-     l=[]
-     for i in distances[1:6]:
-          l.append("{}".format(model.iloc[i[0]].title))
-     return(1)
- 
+    st.table(df) 
+    index = model[model['title'] == skill].index[0]
+    distances = sorted(list(enumerate(simi[index])), reverse=True, key=lambda x: x[1])
+    l=[]
+    for i in distances[1:6]:
+        l.append("{}".format(model.iloc[i[0]].title))
+    return(1)
+
+if st.button('Rekomendasi Asisten'):
+    st.write('Skill terkait : ')
+    # st.write(movie_recommend(option),show_url(option))
+    df = pd.DataFrame({
+        'Asisten terkait skill': recomen(option)
+    })
+
 @app.route('/data')
 def caas():
     return lj.g
@@ -26,6 +37,5 @@ def caas():
 def pembimbing(): 
     return dt.obj
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = '192.168.0.101', port = 5000, debug=True)
