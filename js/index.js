0acb9b8a5a9624a1fd238d6cd66d698e8fa4b357
RazorHex <ahda043@gmail.com>
2022-12-12 15:15:08 +0700

    Adding function to search and console.log it

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-const search = () => {
+const search = async() => {
   const dataSearch = document.getElementById('inSearch').value
   console.log(dataSearch)
+  let inf = await data()
+  console.log(inf[dataSearch].name)
 
+}
+
+const data = async () => {
+  const raw = await fetch('assets/data/skill.json')
+  const data = await raw.json()
+  return data
 }
