756af75bd2d3ce432cf77eaf328a8f6f4683dc99
RazorHex <ahda043@gmail.com>
2022-12-12 15:49:41 +0700

    Created logic for search but unable to create new element without refreshing the page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
 const search = async() => {
   const dataSearch = document.getElementById('inSearch').value
-  console.log(dataSearch)
   let inf = await data()
-  console.log(inf[dataSearch].name)
+  let listed = () => {
+    const list = []
+    try{
+      const data = inf[dataSearch].name
+      data.forEach(nama => {
+        const node = document.createTextNode(nama)
+        list.push(node)
+      });
+      return list
+    } catch {
+      list.push(document.createTextNode(`Data '${dataSearch}' not found!`))
+      return list
+    }
+  }
 
+  const node = listed()
+  const parag = document.createElement('p')
+  node.forEach(l => {
+    parag.appendChild(l)
+  })
+  const element = document.getElementById('header')
+  element.appendChild(parag)
+  console.log(listed())
 }
 
 const data = async () => {
