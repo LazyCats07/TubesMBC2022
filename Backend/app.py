df6f35504157d5e492df93618a2b91210c6a9668
Zaidan Mahdigit config --global user.emailzaidanici@gmail.com <zaidanici@gmail.com>
2022-12-16 17:33:08 +0700

    tambah post backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,12 @@
 import psycopg2
-from flask import Flask,jsonify,request,render_template
+from flask import jsonify,Flask,request
 from datetime import datetime
-from flask_restful import Api,Resource,reqparse,abort
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 con = psycopg2.connect(         # buat connect ke database di postgre
-        host = "192.168.0.101",
+        host = '192.168.0.101',
         port = '8888',
         database = 'postgres',
         user = 'zaidan',
@@ -32,16 +32,16 @@ class profil(Resource):
     def post(self) : 
         try :
             sql = con.cursor()
-            _nama = request.form['name'] #tinggal ganti nama colum 
-            _panggil = request.form['nama_panggil'] #tinggal ganti nama coloumn juga 
-            _jeniskelamin = request.form['jenis_kelamin']
+            _nama = request.form['nama'] #tinggal ganti nama colum 
+            _panggil = request.form['panggil'] #tinggal ganti nama coloumn juga 
+            _jeniskelamin = request.form['kelamin']
             _jurusan = request.form['jurusan']
             _angkatan = request.form['angkatan']
             _divisi = request.form['divisi']
             _status = request.form['status']
-            _banyakprojek = request.form['banyak_project']
+            _banyakprojek = request.form['project']
             _skill = request.form['skill']
-            create_value = """INSERT INTO datafix (name,nama_panggil,Jenis_kelamin,jurusan,angkatan,divisi,status,banyak project,skill) VALUES(%s,%s)"""
+            create_value = """INSERT INTO datafix (nama,panggil,kelamin,jurusan,angkatan,divisi,status,project,skill) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
             sql.execute(create_value,(_nama,_panggil,_jeniskelamin,_jurusan,_angkatan,_divisi,_status,_banyakprojek,_skill))
             con.commit()
             result = jsonify(data="User telah ditambahkan")
@@ -75,10 +75,13 @@ class dosen(Resource):
     def post(self) : 
         try :
             sql = con.cursor()
-            _nama = request.form['nama'] #tinggal ganti nama colum 
-            _divisipem = request.form['divisi'] #tinggal ganti nama coloumn juga 
-            create_value = """INSERT INTO pembina (name,divisi) VALUES(%s,%s)"""
-            sql.execute(create_value,(_nama,_divisipem))
+            _nama = request.form['nama'] 
+            _posisi = request.form['posisi'] 
+            _kode = request.form['kode']
+            _nip = request.form['nip']
+            _email = request.form['email']
+            create_value = """INSERT INTO pembina (nama,posisi,kode,nip,email) VALUES(%s,%s,%s,%s,%s)"""
+            sql.execute(create_value,(_nama,_posisi,_kode,_nip,_email))
             con.commit()
             result = jsonify(data="User telah ditambahkan")
             result.status_code = 200
@@ -108,12 +111,34 @@ class skill(Resource):
         finally : 
             sql.close()
 
+    def post(self) : 
+        try :
+            sql = con.cursor()
+            _nama = request.form['nama'] 
+            _skill = request.form['skill'] 
+            create_value = """INSERT INTO skill (nama,skill) VALUES(%s,%s)"""
+            sql.execute(create_value,(_nama,_skill))
+            con.commit()
+            result = jsonify(data="User telah ditambahkan")
+            result.status_code = 200
+            return(result)
+        except Exception as err : 
+            print(err)
+            result = jsonify(data="data gagal")
+            result.status_code = 400
+            return(result)
+        finally : 
+            sql.close()        
+
 
 @app.route('/')
 def home():
-    return '<h1>Tubes MBC</h1><br>Link Data Asisten dan Caas : tambahin /profil<br>Link Data Pembina Lab : tambahin /dosen'
+    return '<h1>Tubes MBC</h1><br>Link Data Asisten dan Caas : tambahin /profil<br>Link Data Pembina Lab : tambahin /dosen<br>Link Skill : tambahin /skill'
 
-api.add_resource(profil,"/profil",endpoint = "profil", methods=['GET'])
-api.add_resource(dosen,"/dosen",endpoint = "dosen", methods=['GET'])
-api.add_resource(skill,"/skill",endpoint = "skill", methods=['GET'])
-app.run(host = "192.168.0.101", port = "5000")
+api.add_resource(profil,"/profil",endpoint = "profil", methods=['GET','POST'])
+api.add_resource(dosen,"/dosen",endpoint = "dosen", methods=['GET','POST'])
+api.add_resource(skill,"/skill",endpoint = "skill", methods=['GET','POST'])
+# app.run(host = "192.168.233.223", port = "5000")
+
+if __name__ == "__main__":
+    app.run(debug=True, host = "192.168.0.101", port = "5000")
